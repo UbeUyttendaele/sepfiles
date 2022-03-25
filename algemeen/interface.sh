9dd801be150f89b920f7b877a34db07e461fe5da
Ube Uyttendaele <ube.uyttendaele@student.hogent.be>
2022-03-25 10:50:24 +0100

    update

--- algemeen/interface.sh
+++ algemeen/interface.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
-echo "Cheching interface file type and configuring it"
-echo "Determening interface name"
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+#example: echo -e "I ${RED}love${NC} Stack Overflow"
+echo -e "Cheching interface file type and configuring it"
+echo -e "Determening interface name"
 ifconfig -a > interfacenaam
 interface=$(cut -d : -f 1 interfacenaam | head -n 1)
 rm interfacenaam
-echo "Interface name found: $interface"
-echo "Configuring interface: $interface"
+echo -e "Interface name found: ${RED}$interface${NC}"
+echo -e "Configuring interface: ${RED}$interface${NC}"
 FILE=/etc/sysconfig/network-scripts/ifcfg-$interface
 
     echo "DEVICE=$interface" > "$FILE.test"
@@ -14,4 +17,5 @@ FILE=/etc/sysconfig/network-scripts/ifcfg-$interface
     echo "IPADDR=192.168.1.34" >> "$FILE.test"
     echo "PREFIX=29" >> "$FILE.test"
     echo "GATEWAY=192.168.1.33" >> "$FILE.test"
-echo "interface file configured"
+    systemctl restart NetworkManager
+echo -e "interface ${RED}$interface${NC} configured"
--- trinity/web.sh
+++ trinity/web.sh
@@ -1,18 +1,19 @@
 #! /bin/bash
 RED='\033[0;31m'
+BLUE='\033[1;34'
 NC='\033[0m' # No Color
 #example: echo -e "I ${RED}love${NC} Stack Overflow"
 
-echo -e "${RED}Installing nginx, php & postgresql${NC}"
+echo -e "${BLUE}Installing ${RED}nginx${BLUE} & ${RED}postgresql${NC}"
 
     dnf install postgresql-server vim nginx -y
-
+echo -e "${BLUE}Initializing database"
     postgresql-setup --initdb &> /dev/null
     systemctl enable --now postgresql
 
     wget -qO /tmp/database.sh https://raw.githubusercontent.com/UbeUyttendaele/sepfiles/main/trinity/Files/database.sh
     sudo -u postgres /tmp/database.sh
-
+echo -e "${BLUE}Installing ${RED}php-fpm v7.2${NC}"
     yum install epel-release yum-utils -y
     yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
     yum-config-manager --enable remi-php72 -y
@@ -21,22 +22,24 @@ echo -e "${RED}Installing nginx, php & postgresql${NC}"
     wget -qO /etc/php-fpm.d/www.conf https://raw.githubusercontent.com/UbeUyttendaele/sepfiles/main/trinity/Files/www.conf
     chown -R root:nginx /var/lib/php
     
-    systemctl enable php-fpm
-    ssystemctl start php-fpm
-
+    systemctl enable --now php-fpm
+echo -e "${BLUE}Installing ${RED}composer${NC}"
     curl -sS https://getcomposer.org/installer | php -- --install-dir=/bin --filename=composer
     rm ./composer-installer.php
+echo -e "${BLUE}Installing ${RED}drupal using composer${NC}"
     composer create-project drupal-composer/drupal-project:8.x-dev /var/www/my_drupal --stability dev --no-interaction
-
+echo -e "${BLUE}Configuring ${RED}nginx${${BLUE}} & ${RED}drupal${NC}"
     wget -qO /etc/nginx/conf.d/192.168.1.35.conf https://raw.githubusercontent.com/UbeUyttendaele/sepfiles/main/trinity/Files/nginxdrupal.conf
     chown nginx:nginx /etc/nginx/conf.d/192.168.1.35.conf
     wget -qO /var/lib/pgsql/data/pg_hba.conf https://raw.githubusercontent.com/UbeUyttendaele/sepfiles/main/trinity/Files/pg_hba.conf
     wget -qO /etc/nginx/nginx.conf https://raw.githubusercontent.com/UbeUyttendaele/sepfiles/main/trinity/Files/nginx.conf
 
     chown -R nginx: /var/www/my_drupal
-
+echo -e "${BLUE}Restarting services"
     systemctl restart nginx
     systemctl restart php-fpm
     systemctl restart postgresql
+echo -e "${BLUE}SettingHostname"
+    hostnamectl set-hostname Trinity --pretty --static --transient
 
     
